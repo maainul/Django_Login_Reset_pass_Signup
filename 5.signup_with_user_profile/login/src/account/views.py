2a611f4df5adbbed27d95aa6a1d3f2a5fdf0cb33
maainul <mainul080@hotmail.com>
2018-10-29 21:56:51 -0400

    user profile with signup

---
+++ 5.signup_with_user_profile/login/src/account/views.py
@@ -0,0 +1,44 @@
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.auth import login, authenticate
+# #from django.contrib.auth.forms import SignUpForm
+# from django.shortcuts import render, redirect
+# from account.forms import SignUpForm
+
+# @login_required
+# def home(request):
+#     return render(request, 'home.html')
+
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('home')
+#     else:
+#         form = SignUpForm()
+#     return render(request, 'signup.html', {'form': form})
+
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from account.forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
